c954a3a8bd0b4dae54d26aaf2e2d03092d8bce25
kyle <kyledpierson@gmail.com>
2020-01-11 17:48:46 -0700

    Added search functionality, did some refactoring of the directory structure

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Content():
+    displayText = {
+        "about": "<p>Snowy Pier Games was started by three brothers with a passion for board games. Through years of "
+                 "experience playing hundreds of different hobby-style tabletop games, we have developed a keen eye for"
+                 " determining what makes games truly fun to play. Now, our mission is to create amazing board and card"
+                 " games for every type of gamer. Whether you're new to gaming or have been in the hobby for awhile, we"
+                 " can provide you with your next great game. Let's play!</p>",
+        "contact": "",
+        "games": "",
+        "home": "",
+        "news": "",
+        "search": "",
+        "games/tatteredtales": "<p> Can you <b>Locate the lost city of El Dorado</b> or <b>Survive on Mars for a "
+                               "month until the rescue shuttle arrives</b>? These are just a couple of the situations "
+                               "you may encounter in the world of Tattered Tales. Compete as teams to create stories "
+                               "to win the votes of your opposing publishing companies. But there’s a catch! You only "
+                               "have limited items available to you! Will you be able to use your random items to "
+                               "help create a “tattered tale”? If so, you may have what it takes to become the Master "
+                               "Storyteller!</p><p>Tattered Tales is a storytelling game where players work in teams "
+                               "(called publishing companies) to create unique stories! As co-authors of the tale, "
+                               "you and your teammates must use a diverse set of random items to create a story that "
+                               "resolves a given situation. There is limited time to figure out how to not only use "
+                               "the items available to you, but also make your story immersive and entertaining. "
+                               "Players shift to new publishing companies each round, but the player who helps create "
+                               "the most captivating stories throughout the game claims the title of Master "
+                               "Storyteller!</p> "
+    }
